39ac89644fc16a1207f8bc35f862cb722fe9f09c
Benz Muircroft <benzmuircroft@gmail.com>
2014-05-26 18:34:41 +0200

    Create n&d.php

---
+++ n&d.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html><html lang=""><head>
+<meta charset="UTF-8">
+<title>JHTML</title>
+<script type="text/javascript">
+	document.createElement('n');
+	document.createElement('d');
+</script>
+<style>
+	body{
+		margin: 0px;
+		}
+	n{
+		font-weight: normal
+		}
+	d{
+		/*div no style*/
+		}
+	#box{
+		position: absolute;
+		top:10px;left: 100px;
+		width: 90px;height: 90px;
+		background: #000;color: #FFF;
+		padding: 5px;
+		}
+</style>
+</head><body>
+  
+	<d id="box">
+		<b>Bold <n>Normal</n></b>
+	</d>
+	
+	
+</body></html>
