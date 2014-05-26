4f8894c4b2b27d42f93671a9bde78f0e8bc6c9dd
Benz Muircroft <benzmuircroft@gmail.com>
2014-05-26 19:29:37 +0200

    Create tre.php

---
+++ tre.php
@@ -0,0 +1,48 @@
+<script type="text/javascript">
+	document.createElement('tre');
+	document.createElement('mt');
+	document.createElement('tt');
+	document.createElement('lt');
+	document.createElement('rt');
+	document.createElement('bt');
+	document.createElement('d');
+</script>
+<style>
+	tre{font-weight: bold;}
+	mt{ float: left;
+		clear: both;
+		}
+	lt{ float: left;
+		clear: both;
+		position: relative;
+		margin-left: 5px;
+		margin-top: 0px;
+		border: none;
+		border-left: 3px solid #B60539;
+		border-bottom: 3px solid #B60539;
+		width: 0px;
+		height: 6px;
+		padding-left: 7px;
+		padding-top: 9px;
+		}
+	lt > *{position: absolute;margin-left: 5px;width: 100px;}
+	lt > *, mt > *{color: #000!important;}
+</style>
+<div style="position: absolute;left: 10px;width: 143px;text-align: left;">
+	<br>Client Community<br>
+	
+	<br>
+	
+	
+	<tre>
+		<mt><a href="legal-terms-and-conditions.php">Terms &amp; Conditions</a></mt>
+		<lt><a href="about-us.php">About Us</a></lt>
+		<lt><a href="legal-privacy-policy.php">Privacy Policy</a></lt>
+		<lt><a href="legal-anti-spam-policy.php">Anti Spam</a></lt>
+		<lt><a href="seo.php">SEO</a></lt>
+		<lt><a href="design.php">Design</a></lt>
+	</tre>
+	
+	
+	
+</div>
