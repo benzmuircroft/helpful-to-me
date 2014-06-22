04e21a0ac5daab0a85d872a5eed7efb4836d2ed5
Benz Muircroft <benzmuircroft@gmail.com>
2014-06-22 19:43:53 +0200

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,28 @@
+//main thread
+
+
+if(typeof(Worker)!==undefined){
+				var w=new Worker('_S/work.js');
+				w.addEventListener('message',function(e){
+  					console.dir(e.data);
+					},false);
+				w.postMessage('k');
+				}
+				
+
+
+
+			
+//work.js
+
+
+
+self.importScripts(
+	"external1.js",
+	"external2.js"
+	);
+
+self.addEventListener('message',function(e){
+    if(e.data==='k'){
+		  self.postMessage(JSON.stringify({'some':'thing'}));
+		  }});
