db4a70bc982ef6c94cacfdc8dbd3092bd85ddd12
Benz Muircroft <benzmuircroft@gmail.com>
2022-12-10 13:17:39 +0000

    Update tricks.js

--- tricks.js
+++ tricks.js
@@ -1,10 +1,3 @@
-//copy a simple variable
-
-function copy(x){return JSON.stringify(x);}
-
-
-
-
 //using closeure to pass a nulled variable into a function
 
 
@@ -12,12 +5,12 @@ function copy(x){return JSON.stringify(x);}
 (function(){
 
 
-  function next(private){console.log(private);}
+  function next(private){console.log(private);}//shows hidden key
 
   function transportprivate(){
-    var hidden='f6e4cd3ab2a9f84c8cbff644b384130f84d7ef79b60234301de5cb5c82d7';
+    var hidden='f6e4cd3ab2a9f84c8cbff644b384130f84d7ef79b60234301de5cb5c82d7';//key could be got via websocks so not seen in the code
     (function(){var cp=JSON.stringify(hidden);hidden=null;next(cp);})();
     }
-  
+  transportprivate();
   
   })();
